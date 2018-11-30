1fa964df3fe0552e3d3769000e192dc2b2a916b6
AyatsujiP <sample@example.com>
2018-12-01 02:16:15 +0900

    [add]
    add Django Models

--- swiss/settings.py
+++ swiss/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'swiss_gui.apps.SwissGuiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'swiss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'swiss_gui',
+        'USER': 'swiss',
+        'PASSWORD': 'swiss',
+        'HOST':'127.0.0.1',
+        'PORT': '5432'
     }
 }
 
---
+++ swiss_gui/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SwissGuiConfig(AppConfig):
+    name = 'swiss_gui'
