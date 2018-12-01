b41ecd68057a44c91735c01d3daa5e0e855a2160
AyatsujiP <sample@example.com>
2018-12-01 12:00:35 +0900

    [modify]
    modify .gitignore

--- tools/init_database.sql
+++ tools/init_database.sql
@@ -1,10 +1,4 @@
-create table if not exists initial_player_list (
-    id SERIAL,
-    name varchar(128) NOT NULL,
-    rating INTEGER NOT NULL
-);
-
-insert into initial_player_list (name, rating) values 
+insert into swiss_gui_initial_player_list (name, rating) values 
 ('Shiki', 2500),
 ('Karen', 2200),
 ('Frederica', 2000),
