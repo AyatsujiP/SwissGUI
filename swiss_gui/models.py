d9c8e8fbf8d20c51a3d8c16ad348d2e2d0126c9e
AyatsujiP <sample@example.com>
2018-12-01 12:18:34 +0900

    [add]
    add models

--- swiss_gui/models.py
+++ swiss_gui/models.py
@@ -5,3 +5,24 @@ from django.db import models
 class InitialPlayerList(models.Model):
     name = models.CharField(max_length=200)
     rating = models.IntegerField()
+    
+class ParticipatedPlayerList(models.Model):
+    name = models.CharField(max_length=200)
+    rating = models.IntegerField()
+    pairing_no = models.IntegerField()
+    score = models.FloatField()
+    float_status = models.IntegerField()
+
+class CurrentRoundPlayerList(models.Model):
+    name = models.CharField(max_length=200)
+    rating = models.IntegerField()
+    pairing_no = models.IntegerField()
+    score = models.FloatField()
+    float_status = models.IntegerField()
+    
+class Round(models.Model):
+    round_no = models.IntegerField()
+    
+class PooledResults(models.Model):
+    name = models.CharField(max_length=200)
+    result = models.FloatField()
